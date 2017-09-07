49a8efe21aebab483d5c756577f909c69dbc608f
subtopic <dbritt@csumb.edu>
2017-09-07 15:05:03 -0700

    updated doc

--- movieClass.py
+++ movieClass.py
@@ -1,9 +1,4 @@
-"""
-Movie Database
-~~~~~~~~~~
-*fullstack_moviedb* is a simple tool that grabs movie information from "The Movie Database" located themoviedb.org.
-This tool then creates an array of the information, spits it into a file that creates a visual web experience of the information.
-"""
+
 
 __title__ = 'fullstack_moviedb'
 __version__ = '1.0.0'
@@ -12,6 +7,9 @@ __author__ = 'Daniel Britt'
 import webbrowser
 
 class Movie():
+    """*fullstack_moviedb* is a simple tool that grabs movie information from "The Movie Database" located themoviedb.org.
+    This tool then creates an array of the information, spits it into a file that creates a visual web experience of the information."""
+
     def __init__(self, movieName, movieStoryline, posterImage, trailerLink, rating):
         self.title = movieName
         self.storyline = movieStoryline
